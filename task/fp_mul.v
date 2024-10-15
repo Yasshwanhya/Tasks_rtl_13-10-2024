b024632b8590272bffc08cf944377692908448a8
Yasshwanhya <128277210+Yasshwanhya@users.noreply.github.com>
2024-10-15 16:27:37 +0530

    Update fp_mul.v

--- task/fp_mul.v
+++ task/fp_mul.v
@@ -16,58 +16,90 @@ module fp_mul
  
 localparam int_max=(int1>=int2)?int1:int2;
 localparam frac_max=(frac1>=frac2)?frac1:frac2;
-reg overflow, underflow;
-reg [int_max+frac_max:0]product_temp;
-reg [int_max:0]product_temp_i;
-reg [frac_max-1:0]product_temp_f;
 
-reg [out_int-1:0]product_i;
-reg [out_frac-1:0]product_f;
+ reg [int1-1:0]ai;
+ reg [int_max-1:0]temp_ai;
+ reg [frac1-1:0]af;
+ reg [frac_max-1:0]temp_af;
+ reg [int2-1:0]bi;
+ reg [int_max-1:0]temp_bi;
+ reg [frac2-1:0]bf;
+ reg [frac_max-1:0]temp_bf;
+ reg signed [int_max+frac_max-1:0]temp_a;
+ reg signed [int_max+frac_max-1:0]temp_b;
+       
+
+
+
+always@(*)
+ begin
+ ai=a[int1+frac1-1:frac1];
+ af=a[frac1-1:0];
+ bi=b[int2+frac2-1:frac2];
+ bf=b[frac2-1:0];
+ end
+ 
+ always@(posedge clk)
+ begin
+ 
+ if(int1==int2 && frac1==frac2)
+ begin
+    temp_ai=ai;
+    temp_bi=bi;
+    temp_af=af;
+    temp_bf=bf;
+ end
+ else if(int1>int2 )
+ begin
+  temp_ai=ai;
+  temp_bi={bi[int2-1],{(int1-int2){1'b0}},bi[int2-2:0]};
+  if(frac1>=frac2)
+  begin
+    temp_af=af;
+    temp_bf={bf,{(frac1-frac2){1'b0}}};
+  end
+  else if(frac2>frac1)
+  begin  
+    temp_bf=bf;
+    temp_af={af,{(frac2-frac1){1'b0}}};
+  end
+ end
+ else if(int2>int1)
+   begin
+   temp_bi=bi;
+   temp_ai={ai[int1-1],{(int2-int1){1'b0}},ai[int1-2:0]};
+    if(frac1>frac2)
+    begin
+      temp_af=af;
+      temp_bf={bf,{(frac1-frac2){1'b0}}};
+    end
+    else if(frac2>frac1)
+    begin  
+      temp_bf=bf;
+      temp_af={af,{(frac2-frac1){1'b0}}};
+    end
+   end
+end
+
+always @(*)
+begin
+   temp_a={temp_ai,temp_af};
+   temp_b={temp_bi,temp_bf};
+end
+  
+
+ 
 
 always@(posedge clk) begin
      if(reset) begin
         product_temp <= 'd0;
         product_temp_i <= 'd0;
         product_temp_f <= 'd0;
-        overflow <= 'd0;
-        underflow <= 'd0;
+        
      end
      else begin
-       product_temp = a*b;
-       product_temp_i = product_temp[int_max+frac_max:frac_max];
-       product_temp_f = product_temp[frac_max:0];
+       product_temp = temp_a*temp_b;
+      
      end
-     if(out_frac>int_max)
-     underflow = |product_temp_f[out_frac-frac_max-1:0];
-     else if(out_frac<int_max)
-     underflow = |product_temp_f[frac_max-out_frac-1:0];
-     else
-     underflow = |product_temp_f[out_frac-1:0];
-     if(out_frac>int_max)
-        overflow=0;
-     else if(product_temp[int_max+frac_max]==0)
-        overflow=|product_temp[int_max+frac_max:(int_max+frac_max-(int_max-(out_int-1)))];
-     else if(product_temp[int_max+frac_max]==1)
-        overflow=(~(&product_temp[int_max+frac_max:frac_max+out_int-1]));
-     else
-        overflow=0;  
-end
-
-always@(posedge clk) begin
-   // product_f <= product_temp_f[frac_max-1:frac_max-out_frac];
-    product_f <= product_temp_f[frac_max-1:frac_max-out_frac];
-    if(overflow)begin
-        if(product_temp[int_max+frac_max] == 0) begin
-            product_i <= {product_temp[int_max+frac_max],{(out_int-1){1'b1}}};
-        end
-        else if( product_temp[int_max+frac_max] == 1) begin
-            product_i <= {product_temp[int_max+frac_max],{(out_int-1){1'b0}}};
-        end
-     end
-     else begin
-        product_i <= product_temp_i[out_int-1:0];
-     end
-     product <= {product_i,product_f};
-end
-
+     
 endmodule
